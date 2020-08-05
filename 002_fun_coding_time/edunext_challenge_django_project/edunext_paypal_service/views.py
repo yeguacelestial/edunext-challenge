ff71be8c8f177ac5e4009c2009d8580d3f8f909a
Yegua Celestial <yeguacelestial@gmail.com>
2020-08-05 18:19:49 -0500

    Figured out how to manipulate JSON data of Customers

--- 002_fun_coding_time/edunext_challenge_django_project/edunext_paypal_service/views.py
+++ 002_fun_coding_time/edunext_challenge_django_project/edunext_paypal_service/views.py
@@ -2,13 +2,21 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 # DRF
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import PaypalSerializer
 
+# Models
 from .models import Paypal
 
-# Create your views here.
+# For sending requests to Customer API
+import requests
+
+# # SETTINGS # #
+CUSTOMER_API_ENDPOINT = 'http://localhost:8010/api/v1/customerdata/'
+
+# # VIEWS # #
 
 # Manipulate GET request
 @api_view(['GET'])
@@ -20,14 +28,82 @@ def ipnNotif_list(request):
     return Response(serializer.data)
 
 
-# Manipulate POST request
+# Manipulate POST request on this API
 @api_view(['POST'])
 def ipnNotif_create(request):
     # request.data object is a JSON object
     serializer = PaypalSerializer(data=request.data)
 
-    # Validate that we have every field on the request
+    # IF all fields are available on the POST request:
     if serializer.is_valid():
-        serializer.save()
 
-    return Response(serializer.data)
+        # IF payment_status IS Completed:
+        if request.data['payment_status'] == 'Completed':
+            # Current available items
+            available_items = [
+                'free',
+                'basic',
+                'premium'
+            ]
+
+            # IF item_name IN available_items:
+            if request.data['item_name'] in available_items:
+                # Send the info to the Customer API
+                print('[+] VALIDATED ITEM NAME')
+                customerAPI_update(request, payment_is_completed=True)
+
+                # Save the IPN Notif Object in the DB
+                serializer.save()
+                return Response(serializer.data)
+
+            # ELSE if item_name IS NOT IN available_items:
+            else:
+                json_detail = {
+                    'error': 'Item name is not valid.'
+                }
+                print("[-] ERROR => ITEM NAME IS NOT VALID")
+                return Response(status=status.HTTP_406_NOT_ACCEPTABLE, data=json_detail)
+
+        # ELSE IF payment_status IS NOT "Completed":
+        else:
+            customerAPI_update(request, payment_is_completed=False)
+
+    # If the request is not valid, return a HTTP error
+    else:
+        json_error = {
+            'error': 'Invalid request.'
+        }
+        return Response(data=json_error, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+# TODO
+# Update data on Customer API
+def customerAPI_update(request, payment_is_completed:bool):
+    # IF payment_status IS Completed:
+    if payment_is_completed == True:
+
+        # Update info on the Customer API
+        item_name = request.data['item_name']
+        payer_id = request.data['payer_id']
+        payer_endpoint_url = CUSTOMER_API_ENDPOINT + payer_id + '/'
+
+        print(f"[+] REQUEST FROM PAYER {payer_id} RECEIVED, UPDATING DATA ON CUSTOMER API...")
+
+        # Get json object from Customer request
+        customer_json_object = requests.get(payer_endpoint_url).json()
+
+        # Update data of Customer
+        customer_json_object['data']['SUBSCRIPTION'] = 'basic'
+
+        # Send the modified JSON object
+        put_customer_data = requests.put(payer_endpoint_url, json=customer_json_object)
+        print(put_customer_data)
+        
+    # ELSE IF payment_status IS DIFFERENT THAN completed
+    else:
+        # On CustomerAPI[payer_id]:
+            # Update SUBSCRIPTION field to free
+            # Update SUBSCRIPTION field to free
+            # Update all elements of ENABLED_FEATURES to False
+        pass
+
+    return
